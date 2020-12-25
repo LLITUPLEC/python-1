25f33b11665cb30bc1889ffd8ed249fb8818d9b8
LLITUPLEC <46906379+LLITUPLEC@users.noreply.github.com>
2020-12-25 08:07:09 +0300

    [Урок №1] Задача 1

---
+++ lesson-1/1.py
@@ -0,0 +1,11 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран, запросите у пользователя несколько чисел
+# и строк и сохраните в переменные, выведите на экран.
+
+name = 'Vasya Pupkin'
+age = 99
+print(name)
+print(age)
+user_name = input('Inter your name: ')
+user_age = input('Inter your age: ')
+print(f'Ваше имя - {user_name}')
+print(f'Ваш возраст - {user_age}')
